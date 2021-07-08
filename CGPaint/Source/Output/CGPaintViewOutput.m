0f384af9a9924942e486459de31464ac165e5fa7
Jason <jasonweixin@163.com>
2021-07-08 11:04:33 +0800

    代码整理

--- CGPaint/Source/Output/CGPaintViewOutput.m
+++ CGPaint/Source/Output/CGPaintViewOutput.m
@@ -58,46 +58,6 @@
     }
 }
 
-//
-//- (UIImage*) takePicture {
-//    int s = 1;
-//    UIScreen* screen = [UIScreen mainScreen];
-//    if ([screen respondsToSelector:@selector(scale)]) {
-//        s = (int) [screen scale];
-//    }
-//
-//    GLint viewport[4];
-//    glGetIntegerv(GL_VIEWPORT, viewport);
-//
-//
-//    int width = viewport[2];
-//    int height = viewport[3];
-//
-//    int myDataLength = width * height * 4;
-//    GLubyte *buffer = (GLubyte *) malloc(myDataLength);
-//    GLubyte *buffer2 = (GLubyte *) malloc(myDataLength);
-//    glReadPixels(0, 0, width, height, GL_RGBA, GL_UNSIGNED_BYTE, buffer);
-//    for(int y1 = 0; y1 < height; y1++) {
-//        for(int x1 = 0; x1 <width * 4; x1++) {
-//            buffer2[(height - 1 - y1) * width * 4 + x1] = buffer[y1 * 4 * width + x1];
-//        }
-//    }
-//    free(buffer);
-//
-//    CGDataProviderRef provider = CGDataProviderCreateWithData(NULL, buffer2, myDataLength, NULL);
-//    int bitsPerComponent = 8;
-//    int bitsPerPixel = 32;
-//    int bytesPerRow = 4 * width;
-//    CGColorSpaceRef colorSpaceRef = CGColorSpaceCreateDeviceRGB();
-//    CGBitmapInfo bitmapInfo = kCGBitmapByteOrderDefault;
-//    CGColorRenderingIntent renderingIntent = kCGRenderingIntentDefault;
-//    CGImageRef imageRef = CGImageCreate(width, height, bitsPerComponent, bitsPerPixel, bytesPerRow, colorSpaceRef, bitmapInfo, provider, NULL, NO, renderingIntent);
-//    CGColorSpaceRelease(colorSpaceRef);
-//    CGDataProviderRelease(provider);
-//    UIImage *image = [ UIImage imageWithCGImage:imageRef scale:s orientation:UIImageOrientationUp ];
-//    return image;
-//}
-
 - (void)setClearColorRed:(GLfloat)red green:(GLfloat)green blue:(GLfloat)blue alpha:(GLfloat)alpha {
     bgColor[0] = red;
     bgColor[1] = green;
